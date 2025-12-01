1914d7fff94492e05df4813160ff857460af33a8
zangzoo <jangjang0022@gmail.com>
2025-12-01 17:22:29 +0900

    💄 Name change (예린 -> 수정)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ import AlbumSidebar from "../components/AlbumSidebar";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  const [username] = useState("예린");
+  const [username] = useState("수정");
   const navigate = useNavigate();
   const bookListRef = useRef(null);
   const [canScrollLeft, setCanScrollLeft] = useState(false);
