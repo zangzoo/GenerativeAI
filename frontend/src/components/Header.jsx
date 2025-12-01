78157f4dd71fb2399e45d153e43085ff186bc2f0
zangzoo <jangjang0022@gmail.com>
2025-12-01 17:22:08 +0900

    💄 UI change

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import "../styles/Header.css";
 export default function Header() {
   return (
     <header className="header">
-      <h1>ReadingMate</h1>
+      <h1>Reading Mate</h1>
     </header>
   );
 }
